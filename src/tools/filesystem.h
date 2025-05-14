564b156a190b2b56d43437fbaab9bf448a80f2d2
shediao.xsd <xushediao1987@163.com>
2025-05-14 23:07:59 +0800

    简化维护 tools function定义, json文件编译时嵌入到变量中

--- src/tools/filesystem.h
+++ src/tools/filesystem.h
@@ -2,7 +2,7 @@
 #define __AI_CLI_FILESYSTEM_TOOLS_H__
 
 #include "nlohmann/json.hpp"
-const std::string& get_filesystem_tools();
+const std::string_view get_filesystem_tools();
 
 void regist_filesystem_tools();
 #endif  // __AI_CLI_FILESYSTEM_TOOLS_H__
