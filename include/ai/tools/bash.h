4bcfd5e7ef26778eeac57051e23b10e94aaf1c9a
shediao.xsd <xushediao1987@163.com>
2026-05-20 12:53:48 +0800

    refactor: remove unused tool_calls headers, bash.h, default.h

--- include/ai/tools/bash.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-const std::string_view get_bash_tools();
-void regist_bash_tools();
--- include/ai/tools/default.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-const std::string_view get_default_tools();
-void regist_default_tools();
