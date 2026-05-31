598e05ced53041e467eacc812875bd2d5c143533
shediao.xsd <xushediao1987@163.com>
2026-05-31 12:12:07 +0800

    refactor: suppress shared_ptr atomic deprecation warning locally instead of globally
    Move the _SILENCE_CXX20_OLD_SHARED_PTR_ATOMIC_SUPPORT_DEPRECATION_WARNING
    suppression from a global compile definition in CMakeLists.txt to a local
    #pragma in src/base/timer.h, where the deprecated std::atomic_load/store/
    exchange on std::shared_ptr are actually used.

--- src/base/timer.h
+++ src/base/timer.h
@@ -9,6 +9,17 @@
 
 namespace ai::base {
 
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
 class Timer {
  public:
   Timer() = default;
@@ -121,5 +132,11 @@ class Timer {
   }
 #endif
 };
+#if defined(__clang__) || defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
 
 }  // namespace ai::base
